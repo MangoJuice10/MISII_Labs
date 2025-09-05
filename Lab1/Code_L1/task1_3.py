1cfa75f15cadcf3f7bd930cc694218e0a58643a7
MangoJuice10 <swordmaster10A@gmail.com>
2025-09-05 06:01:48 +0300

    Разобрал метод str.count()

---
+++ Lab1/Code_L1/task1_3.py
@@ -0,0 +1,4 @@
+haystack = 'Хороший код — это читаемый код.'
+needle = 'код'
+count = haystack.count(needle)
+print(f'''Сколько раз слово "{needle}" содержится в предложении "{haystack}"?\n{count}''')
